4ac5bef79bfdf25d1b8d68ea86f5a50de3c0f1da
Zev Stravitz <zevstravitz@verizon.net>
2020-05-27 13:38:33 -0400

    camera complex

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+'use strict';
+import React, { PureComponent, useState, useEffect } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import Permissions from 'react-native-permissions';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const App = () => {
+    let [flash, setFlash] = useState('off')
+    let [zoom, setZoom] = useState(0)
+    let [autoFocus, setAutoFocus] = useState('on')
+    let [depth, setDepth] = useState(0)
+    let [type, setType] = useState('back')
+    let [permission, setPermission] = useState('undetermined')
+    let cameraRef = useRef(null)
+    
+    
+    useEffect(() => {
+        Permissions.check('photo').then(response => {
+        // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+        setPermission(response);
+        });
+    }, []);
+
+    function toggleFlash () {
+        setFlash(flashModeOrder[flash])
+    }
+    function zoomOut () {
+        setZoom(zoom - 0.1 < 0 ? 0 : zoom - 0.1)
+    }
+    function zoomIn () {    
+        setZoom(zoom + 0.1 > 1 ? 1 : zoom + 0.1);
+    }
+    takePicture = async() => {
+        if (cameraRef) {
+            const options = { quality: 0.5, base64: true };
+            const data = await cameraRef.current.takePictureAsync(soptions);
+            console.log(data.uri);  
+        }
+    };
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={cameraRef}
+          style={styles.preview}
+          type={type}
+          flashMode={flash}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={takePicture} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
